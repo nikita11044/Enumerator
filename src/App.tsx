156f7eab0c9dc0cc2acfa5fe94686924727630b7
Nikita Inskorostinskii <62565112+nikita11044@users.noreply.github.com>
2021-02-07 01:20:32 +0300

    Initiated a project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className='panel settings-panel'>
+                <div className='monitor'>
+                    <form className='value-display'>
+                        <label className='max-value-desrc'>max value:   <input type='number' value="0" className='max-value-num'/></label>
+                        <label className='min-value-desrc'>min value:   <input type='number' value="0" className='min-value-num'/></label>
+                    </form>
+                </div>
+                <div className='button-wrapper'>
+                    <button className='button'>set</button>
+                </div>
+            </div>
+            <div className='panel number-panel'>
+                <div className='monitor'>
+                    <div className='number-display'>0</div>
+                </div>
+                <div className='button-wrapper'>
+                    <button className='button'>inc</button>
+                    <button className='button'>reset</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
